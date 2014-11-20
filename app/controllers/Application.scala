b1d40c6df9f7da24b4ebb5b6648b88aec8cdc10d
darioalessandro <darioalessandrolencina@gmail.com>
2014-11-20 16:08:13 -0500

    added exception handler  in case that one of the Futures throws an exception

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,17 @@
 package controllers
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{TimeoutException, Executors}
 
 import org.joda.time.{DateTimeZone, DateTime}
 import play.api._
 import play.api.mvc._
 import play.api.Play.current
 import play.api.libs.ws._
+import play.libs.F
 import scala.concurrent.{Promise, ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
 
 object Application extends Controller {
 
@@ -28,19 +31,24 @@ object Application extends Controller {
 
   def testMultipleRequests=Action.async {
 
+    val timeoutFuture = play.api.libs.concurrent.Promise.timeout("Timeout", 10 seconds)
+
     val futures:List[Future[WSResponse]] = List( "http://www.google.com", "http://www.chevrolet.com.mx","http://www.yahoo.com", "http://www.clinique.com", "http://www.mvsnoticias.com") map {
-      case url:String => WS.url(url).get
+      case url:String =>
+        val future=WS.url(url)
+        future.get()
     }
 
-    futures map (future=> future.onComplete {
-      case Success(wsResponse) => println( s" ${wsResponse.header("Set-Cookie")} Completed on: ${new DateTime( DateTimeZone.UTC )}")
-      case Failure(t) => println("An error has occured: " + t.getMessage)
-    })
-
-    sequence(futures) map {
+    Future.firstCompletedOf(Seq(sequence(futures), timeoutFuture)).map {
       case responses:List[WSResponse]=>
         val responsesStatus= responses map (response => response.statusText)
         Ok(s"Responses $responsesStatus")
+      case t: String => InternalServerError(t)
+    } recoverWith {
+      case error=>
+        Future {
+          Ok(s"Ya valió $error")
+        }
     }
 
   }
